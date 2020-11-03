610e4af61b05a12a4dd3cf222570b1fdf24e3654
MCwindTIM <pw860563576@gmail.com>
2020-11-04 05:50:35 +0800

    Update imposter.js

--- Commands/Test/imposter.js
+++ Commands/Test/imposter.js
@@ -15,35 +15,21 @@ module.exports = class ImposterCommand extends Command {
         if(!imposter){
             imposter = message.author;
         }
-        let line;
-        if(imposter.username.length <= 15){
-            line = `        　　        ${imposter.username} was The Impostor.　 。　.`;
-        }else{
-            line = `        　　       ${imposter.username}\n        　　         was The Impostor.　 。　.`.substring(imposter.username.length - 6);
-        }
-        let pattern = `. 　　　。　　　　•　 　ﾟ　　。 　　.
+        let line = `        　　  ${imposter.username} was The Impostor.　 。　.`;;
+        let pattern = `
+        . 　　　。　　　　•　 　ﾟ　　。 　　.
 
         　　　.　　　 　　.　　　　　。　　 。　. 　
-        
+
         .　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
-        
-        ${line}
-        
+
+${line}
+
         　　'　　　 1 Impostor remains 　 　　。
-        
+
         　　ﾟ　　　.　　　. ,　　　　.`;
 
         
         message.channel.send(pattern);
     }
-
-    pad(pad, str, padLeft) {
-        if (typeof str === 'undefined') 
-          return pad;
-        if (padLeft) {
-          return (pad + str).slice(-pad.length);
-        } else {
-          return (str + pad).substring(0, pad.length);
-        }
-      }
 }
