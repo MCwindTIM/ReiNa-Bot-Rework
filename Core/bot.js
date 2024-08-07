b15c43adf5b4a15965091b94ca586235e24f55b5
MCwindTIM <ryancheng5542@gmail.com>
2024-08-07 18:51:30 +0800

    add simple alive api

--- Core/bot.js
+++ Core/bot.js
@@ -3,6 +3,7 @@ const Config = require("./config");
 const Util = require("./utils");
 const Events = require("./events");
 const Rows = require("./rows")
+const express = require('express');
 
 //音樂依賴
 const { DisTube } = require('distube')
@@ -10,9 +11,15 @@ const { SpotifyPlugin } = require('@distube/spotify')
 const { SoundCloudPlugin } = require('@distube/soundcloud')
 const { YtDlpPlugin } = require('@distube/yt-dlp')
 
+const { YouTubePlugin } = require("@distube/youtube")
+
 module.exports = class ReiNaRework{
     constructor(option){
         this.config = new Config(this);
+        this.webApp = express();
+        this.webApp.get('/', (req,res) => {
+            res.send("I'm alive!");
+        })
         this.bot = new Discord.Client({ intents: [
             Discord.GatewayIntentBits.Guilds,
             Discord.GatewayIntentBits.GuildMessages,
@@ -23,24 +30,23 @@ module.exports = class ReiNaRework{
             Discord.GatewayIntentBits.MessageContent
         ]});
         this.bot.distube = new DisTube(this.bot, {
-            leaveOnStop: false,
             emitNewSongOnly: true,
             emitAddSongWhenCreatingQueue: false,
             emitAddListWhenCreatingQueue: false,
             plugins: [
-                new SpotifyPlugin({
-                    emitEventsAfterFetching: true
-                }),
+                new SpotifyPlugin(),
                 new SoundCloudPlugin(),
-                new YtDlpPlugin()
+                new YouTubePlugin({
+                    cookies: this.config.youtubeCookie
+                })
             ],
-            youtubeCookie: this.config.youtubeCookie
         })
         
         this.util = new Util(this);
         this.events = new Events(this);
         this.rows = new Rows(this);
         this.queue = new Map();
+
         this.util.load(this).then((data) => {
             this.commands = data.commands;
             this.buttons = data.buttons;
@@ -48,5 +54,11 @@ module.exports = class ReiNaRework{
             this.loginTime = "";
             this.bot.login(this.config.token);
         })
+        this.httpServer = this.webApp.listen(this.config.Port, () => {
+            var host = this.httpServer.address().address
+            var port = this.httpServer.address().port
+            console.log(`📶網頁伺服器正在監聽! 訪問地址為http://${host + port}`)
+        });
+
     }
 }
